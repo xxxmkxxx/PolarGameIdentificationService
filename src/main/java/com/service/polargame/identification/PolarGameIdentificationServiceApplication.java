51f32cafd180d037f5e8bda9139c4d88f625b347
Max <maks88648@gmail.com>
2024-04-15 14:03:40 +0400

    init

---
+++ src/main/java/com/service/polargame/identification/PolarGameIdentificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.polargame.identification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarGameIdentificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolarGameIdentificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/polargame/identification/PolarGameIdentificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.polargame.identification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarGameIdentificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
